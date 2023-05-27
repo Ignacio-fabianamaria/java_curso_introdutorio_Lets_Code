336b6223c7ab4798e302680554c3d74225e82141
Siigmaf <faby.tbe@gmail.com>
2023-05-27 13:46:13 -0300

    feat:conceitos introdutórios sobre variáveis

---
+++ variaveis/index.java
@@ -0,0 +1,18 @@
+package variaveis;
+
+public class index {
+    public static void main(String[] args) {
+        String nome = "Fabiana";
+        System.out.println("Olá " + nome + " !");
+
+        int a = 3, b=2;
+        int soma = a + b;
+        int sub = a - b;
+        int mult = a * b;
+        float div = (float) a / b;
+        System.out.println(a+" + "+b+" = "+soma);
+        System.out.println(a+" + "+b+" = "+sub);
+        System.out.println(a+" + "+b+" = "+mult);
+        System.out.println(a+" + "+b+" = "+div);
+    }
+}
