ace4bbab251752696fc89761742d50971202cb25
Siigmaf <faby.tbe@gmail.com>
2023-05-27 18:16:12 -0300

    feat: lações de repetição - tabuáda

---
+++ lacos_de_repeticao/index.java
@@ -0,0 +1,13 @@
+package lacos_de_repeticao;
+
+public class index {
+    public static void main(String[] args) {
+        // tabuada de "X"
+
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <=10; j++) {
+                System.out.println(j+ " X " + i+ " = " + j * i);
+            }
+        }
+    }
+}
